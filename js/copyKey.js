015496418d9f3958d199e8127abc349ce54c7704
Solodovnikov Ilya <solilya2604@yandex.ru>
2024-10-06 22:23:39 +0300

    copyKey.js improved

--- js/copyKey.js
+++ js/copyKey.js
@@ -1,23 +1,21 @@
-// copyKey.js
-
 document.addEventListener('DOMContentLoaded', function() {
     const copyButton = document.getElementById('copy_key');
     if (copyButton) {
         copyButton.addEventListener('click', function() {
             const keyInput = document.getElementById('api_key');
-            keyInput.select();
-            keyInput.setSelectionRange(0, 99999); // Для мобильных устройств
 
-            // Копирование текста
-            document.execCommand('copy');
-
-            // Временное уведомление об успешном копировании
-            copyButton.textContent = 'Скопировано!';
-            copyButton.disabled = true;
-            setTimeout(() => {
-                copyButton.textContent = 'Скопировать';
-                copyButton.disabled = false;
-            }, 2000);
+            // Копирование текста с помощью Clipboard API
+            navigator.clipboard.writeText(keyInput.value).then(() => {
+                // Временное уведомление об успешном копировании
+                copyButton.textContent = 'Скопировано!';
+                copyButton.disabled = true;
+                setTimeout(() => {
+                    copyButton.textContent = 'Скопировать';
+                    copyButton.disabled = false;
+                }, 2000);
+            }).catch(err => {
+                console.error('Ошибка при копировании:', err);
+            });
         });
     }
 });
